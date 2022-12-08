1a0526d5f58b93dd7a43e68fe794829fe2216ca2
ibarretom <igorbm.23@gmail.com>
2022-12-07 23:58:57 -0300

    Configs do manifest.jsomn

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,16 @@ module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
   ],
-  lintOnSave: false
+  lintOnSave: false,
+  pwa: {
+    name: 'Descobrindo o bullying',
+    short_name: 'Descobrindo o bullying',
+    themeColor: '#8C251C',
+    msTileColor: '#8C251C',
+    background_color: '#8C251C',
+    appleMobileWebAppStatusBarStyle: '#8C251C',
+    manifestOptions: {
+      background_color: '#8C251C'
+    }
+  }
 })
