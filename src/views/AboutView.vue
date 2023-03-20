7a6aecfcc0bc8f1f5aba7a4711c8db672380d7e2
ibarretom <igorbm.23@gmail.com>
2023-03-20 02:16:55 -0300

    Mudança foto da página inicial

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,14 +1,28 @@
 <template>
-  <v-container class="fill-height d-flex flex-column justify-space-between content-size">
+  <v-container
+    class="fill-height d-flex flex-column justify-space-between content-size"
+  >
     <div class="full-width">
-      <v-img src="../assets/images/bullyingGame/end.png" max-height="212" max-width="328" cover
-        class="bg-grey-lighten-2 rounded-lg mx-auto"></v-img>
+      <v-img
+        src="../assets/images/bullyingGame/end.png"
+        max-height="212"
+        max-width="328"
+        cover
+        class="bg-grey-lighten-2 rounded-lg mx-auto"
+      ></v-img>
 
-      <p class="mt-3">Quero saber mais: <a class="primary--text"
-          @click="goToVideo1">https://www.youtube.com/watch?v=Oi3K9KDt_FY</a></p>
-      <p>Para pais e professores: <a class="primary--text"
-          @click="goToVideo2">https://www.youtube.com/watch?v=21mDekTZwsw</a></p>
-
+      <p class="mt-3">
+        Quero saber mais:
+        <a class="primary--text" @click="goToVideo1"
+          >https://www.youtube.com/watch?v=Oi3K9KDt_FY</a
+        >
+      </p>
+      <p>
+        Para pais e professores:
+        <a class="primary--text" @click="goToVideo2"
+          >https://www.youtube.com/watch?v=psieH5qBIpk</a
+        >
+      </p>
     </div>
     <div class="full-width grid-3" v-if="showButtons">
       <div class="d-flex flex-column align-center" @click="handleButton">
@@ -38,20 +52,20 @@ export default {
   name: "AboutView",
 
   data: () => ({
-    showButtons: true
+    showButtons: true,
   }),
   methods: {
     handleButton() {
-      this.showButtons = false
+      this.showButtons = false;
     },
     goToVideo1() {
-      window.open('https://www.youtube.com/watch?v=M7wqlF3YM_g', '_blank');
+      window.open("https://www.youtube.com/watch?v=Oi3K9KDt_FY", "_blank");
     },
     goToVideo2() {
-      window.open('https://www.youtube.com/watch?v=21mDekTZwsw', '_blank');
-    }
-  }
-}
+      window.open("https://www.youtube.com/watch?v=psieH5qBIpk", "_blank");
+    },
+  },
+};
 </script>
 
 <style scoped>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,32 +1,36 @@
 <template>
-  <v-container class="fill-height d-flex flex-column justify-space-around content-size">
+  <v-container
+    class="fill-height d-flex flex-column justify-space-around content-size"
+  >
     <div class="full-width">
-      <v-img src="../assets/images/bullyingGame/home.png" max-height="212" max-width="328" cover
-        class="bg-grey-lighten-2 rounded-lg mx-auto"></v-img>
-      <article class="text-justify mt-3">
-        VAMOS FALAR SOBRE BULLYING!
-      </article>
+      <v-img
+        src="../assets/images/home.jpeg"
+        cover
+        class="bg-grey-lighten-2 rounded-lg mx-auto"
+      ></v-img>
+      <article class="text-justify mt-3">VAMOS FALAR SOBRE BULLYING!</article>
     </div>
     <div class="full-width">
-      <v-btn @click="pushToGame" class="mt-3" outlined large block rounded dark>Vamos lá?</v-btn>
+      <v-btn @click="pushToGame" class="mt-3" outlined large block rounded dark
+        >Vamos lá?</v-btn
+      >
     </div>
   </v-container>
 </template>
 
 <script>
-
 export default {
   name: "HomeView",
 
   methods: {
     pushToGame() {
-      this.$router.push("jogar")
-    }
+      this.$router.push("jogar");
+    },
   },
   created() {
-    console.log(this.$router)
-  }
-}
+    console.log(this.$router);
+  },
+};
 </script>
 
 <style scoped>
